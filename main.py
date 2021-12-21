6d56d2676b100a8ace9643db28e57f8ee6843015
deeSkumar <90201669+deeSkumar@users.noreply.github.com>
2021-12-21 13:21:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
-low=0
-medium=0
-high=0
-numberofbatteries=int(input("enter the number of batteries"))
-for i in range(numberofbatteries):
-  battery_charge_cycles = int(input("Enter the number of  charge cycles"))
-  if(battery_charge_cycles<150):
-    low=low+1
-  elif(battery_charge_cycles > 150 & battery_charge_cycle < 649):
-    medium=medium+1
-  else:
-    high=high+1
-print("Low count"low)
-print("medium count"medium)
-print("high count"high)
 
 def count_batteries_by_usage(cycles):
+  count1={'lowCount':0,'mediumCount':0,'highCount':0}
+  for cycle in cycles:
+    if (cycle<50):
+      counts1['lowCount']+=1
+    elif (cycle>=150 and cycle<650):
+      counts1['mediumCount']+=1
+    else:
+      counts1['highCount']+=1
   return {
-    "lowCount": 0,
-    "mediumCount": 0,
-    "highCount": 0
+    'lowCount':counts1['lowCount'],
+    'mediumCount':counts1['mediumCount'],
+    'highCount': counts1['highCount']
     
   }
 
@@ -29,6 +22,7 @@ def test_bucketing_by_number_of_cycles():
   assert(counts["lowCount"] == 1)
   assert(counts["mediumCount"] == 3)
   assert(counts["highCount"] == 2)
+  print(counts)
   print("Done counting :)")
 
 
